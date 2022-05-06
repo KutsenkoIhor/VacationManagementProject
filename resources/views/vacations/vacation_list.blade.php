17b76c56752b109465afba583e17d7e7eae3587e
eklerchiik <valeriiaskliarenko@gmail.com>
2022-05-06 19:01:30 +0200

    Fixes

--- resources/views/vacations/vacation_list.blade.php
+++ resources/views/vacations/vacation_list.blade.php
@@ -9,7 +9,7 @@
                 <p class="mt-2 text-sm text-gray-700">A list of all your vacations.</p>
             </div>
             <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-                <a href="/vacations/create" type="button"
+                <a href="/vacations" type="button"
                    class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Create
                     vacation</a>
             </div>
